5a2ac2fe3b6017015f7bb1df6a746868c45e3a12
sntnmjones <sntnmjones@gmail.com>
2017-12-24 09:45:21 -0800

    User form tested and functional.

--- blogs/models.py
+++ blogs/models.py
@@ -1,5 +1,6 @@
 # /blogs/models.py
 from django.db import models
+from django.urls import reverse
 
 class Post(models.Model):
     author = models.ForeignKey(
@@ -9,5 +10,10 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
 
+    """ Takes object and returns it as a string """
     def __str__ (self):
         return self.title
+
+    """ Sends user back to path specified """
+    def get_absolute_url(self):
+        return reverse('home')
